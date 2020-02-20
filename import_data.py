541bb12a6ad3a331185bc90c3b0137cf65e5ecd1
Dustin Swales <dustin.swales@noaa.gov>
2020-02-20 12:11:58 -0700

    Some generalization of code

--- import_data.py
+++ import_data.py
@@ -1,36 +1,73 @@
-#########################################################################################
+##########################################################################################
+#
+# About: This script imports CLAVRx (Cloud from AVHRR extended processing system) from
+#        SSEC to NOAA PSD.
 #
 ##########################################################################################
 from ftplib import FTP
 import os
 from datetime import timedelta, date
+import os.path
+from os import path
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
+        
+##########################################################################################
+# Import configuration
+##########################################################################################
+# What is the name of the FTP server?
+ftpServer = 'ftp.ssec.wisc.edu'
 
-start_date = date(2020, 1, 14)
-end_date = date(2020, 2, 14)
-count = 14
+# Where is the data stored on the FTP server?
+dirFTP = '/pub/clavrx/barbados/'
+
+# Which configuration to download?
+configID = '2km_01min'
+
+# Where should the data be stored locally?
+dirLocal = '/Projects/ATOMIC/data/clavrx/'
+
+# What date range to import?
+start_date = date(2020, 1, 21)
+end_date   = date(2020, 2, 14)
+countDOY   = 21
+
+##########################################################################################
+# Import data
+##########################################################################################
+# Make local root directory (only if it doesn't exist)
+try:
+    os.mkdir(dirLocal+configID)
+except:
+    print("Root directory already exists")
+
+# Loop over all times and pull over data, when available
 for single_date in daterange(start_date, end_date):
-    localDir=single_date.strftime("%Y_%m_%d")+'_'+str(count).zfill(3)
-    count = count+1
+    # On the FTP site the data is archived in directories with the following format.
+    NewTempDir = single_date.strftime("%Y_%m_%d")+'_'+str(countDOY).zfill(3)
+    countDOY   = countDOY+1
 
-    ftp = FTP('ftp.ssec.wisc.edu')
-    print "Welcome: ", ftp.getwelcome()
+    # Login to FTP server
+    ftp = FTP(ftpServer)
     ftp.login()
-    ftp.cwd('/pub/clavrx/barbados/2km_01min/'+localDir+'/')
-    ftp.retrlines('LIST')
-    os.chdir('/data/dswales/Atomic/'+localDir+'/')
-    ls = ftp.nlst()
-    count = len(ls)
-    curr = 0
-    print "found {} files".format(count)
-    for fn in ls:
-        curr += 1
-        print 'Processing file {} ... {} of {} ...'.format(fn, curr, count)
-        ftp.retrbinary('RETR ' + fn, open(fn, 'wb').write)
-    
+    # Is the data we requested on the server?
+    try:
+        ftp.cwd(dirFTP+configID+'/'+NewTempDir+'/')
+        os.mkdir(dirLocal+configID+'/'+NewTempDir+'/')
+        os.chdir(dirLocal+configID+'/'+NewTempDir+'/')
+        ls = ftp.nlst()
+        count = len(ls)
+        curr = 0
+        print "found {} files".format(count)
+        for fn in ls:
+            curr += 1
+            print 'Processing file {} ... {} of {} ...'.format(fn, curr, count)
+            ftp.retrbinary('RETR ' + fn, open(fn, 'wb').write)
+    except:
+        print("Data missing on FTP site: "+NewTempDir)
+        
 ftp.quit()
 print "download complete."
 
