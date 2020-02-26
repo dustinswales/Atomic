d8e14dfae6f2fddb54daf6437252899dd67c3e3a
Dustin Swales <dustin.swales@noaa.gov>
2020-02-26 12:27:13 -0700

    Added script to create movie of cloud-top height

--- extract_case.py
+++ extract_case.py
@@ -3,6 +3,7 @@ import datetime
 import read_data
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 ##########################################################################################
 ##########################################################################################
@@ -11,33 +12,45 @@ def daterange(start_date, end_date):
         yield start_date + datetime.timedelta(n)
 
 ##########################################################################################
+# Animation function
+##########################################################################################
+def animate(i): 
+    z = cld_hgt2d[:,:,i]
+    cont = plt.contourf(lon, lat, z, levels, cmap='YlGnBu')
+    t1 = datetime.datetime(year[i],month[i],day[i],\
+                           hour[i],minute[i])
+    ax.set(title=t1, ylabel='latitude',xlabel='longitude')
+    return cont
+
+levels = np.arange(0, 15000, 1000)
+
+##########################################################################################
 # Configuration
 ##########################################################################################
-
-# Set to true to print information on the files begin read in.
-debug  = 0
-
-# Set true to make plots cloud-fraction when "hot-towers" are detected.
-mkplot = 1
 
 # Data location (OpenDap)
 dirData = 'https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ATOMIC/data/clavrx/2km_01min/'
 
 # What data to read in? [year,month,day,hour,minute]
 t_start = [2020,2,9,21,0]
-t_stop  = [2020,2,9,23,58]
+t_stop  = [2020,2,9,23,59]
 
 # Subset the domain? [lon1,lat1,lon2,lat2]
 sub_extent = [-60,15,-58,17]
 #sub_extent = [] #Using the full domain.
 
-# Here "hot-towers" are defined by a sudden change in the number of pixels with cloud-top
-# height exceeding some threshold.
-# Cloud height threshold
-cld_hgt_thresh   = 6000.
-# Relative change in pixels containing high-clouds.
-changeInPixCount = 0.5
-
+# Name for output movie
+fileOUT = 'hot_towers'       + '_'+ \
+    str(t_start[0]).zfill(4) + \
+    str(t_start[1]).zfill(2) + \
+    str(t_start[2]).zfill(2) + \
+    str(t_start[3]).zfill(2) + \
+    str(t_start[4]).zfill(2) + '_'+ \
+    str(t_stop[0]).zfill(4)  + \
+    str(t_stop[1]).zfill(2)  + \
+    str(t_stop[2]).zfill(2)  + \
+    str(t_stop[3]).zfill(2)  + \
+    str(t_stop[4]).zfill(2)
 
 ##########################################################################################
 # Determine problem size...
@@ -63,7 +76,6 @@ to1 = datetime.datetime(t_start[0], t_start[1], t_start[2])
 tf1 = datetime.datetime(t_stop[0],  t_stop[1],  t_stop[2])
 nDirsToReadFrom = abs(tf1-to1).days+1
 print('Requested time period spans '+str(nDirsToReadFrom)+' days')
-
     
 ##########################################################################################
 # Create file list
@@ -112,12 +124,8 @@ count = 0
 for iTime in range(fileiI,fileiF+1):
     [cld_hgt, cld_mask, lon, lat, error] = read_data.read_data(fileList[iTime], sub_extent)
     if not (error):        
-        if (debug): print('      Reading in '+fileList[iTime])
+        print('      Reading in '+fileList[iTime])
 
-        # What fraction of the domain is covered in high clouds?
-        mask1 = np.where(cld_hgt >= cld_hgt_thresh,1,0)
-        cfp0  = np.sum(mask1)
-        
         # Pull out time information from filename
         tempStr = fileList[iTime]
         si      = len(tempStr)-len(file_suffix)-4
@@ -130,60 +138,34 @@ for iTime in range(fileiI,fileiF+1):
         day0    = int(tempStr[si+8:si+10])
 
         if (init):
-            nlon   = len(lon[:,0])
-            nlat   = len(lat[0,:])
-            nPts   = nlon*nlat
-            year   = year0
-            month  = month0
-            day    = day0
-            hour   = hour0
-            minute = minute0
-            cfp    = cfp0
-            init   = 0
+            nlon      = len(lon[:,0])
+            nlat      = len(lat[0,:])
+            nPts      = nlon*nlat
+            year      = year0
+            month     = month0
+            day       = day0
+            hour      = hour0
+            minute    = minute0
+            cld_hgt2d = np.dstack((cld_hgt,cld_hgt))
+            init      = 0            
         else:
             year   = np.vstack((year,   year0))
             month  = np.vstack((month,  month0))
             day    = np.vstack((day,    day0))
             hour   = np.vstack((hour,   hour0))
             minute = np.vstack((minute, minute0))
-            cfp    = np.vstack((cfp,    cfp0))
-            cld_hgt2d = np.dstack((cld_hgtm1t,cld_hgt))
-        cld_hgtm1t = cld_hgt
-
-
-        # If number of pixels jumps drastically, say 20%?, from subsequent timesteps, make plot
-        if (count > 0):
-            t1 = datetime.datetime(year[count-1],month[count-1],day[count-1],\
-                                   hour[count-1],minute[count-1])
-            t2 = datetime.datetime(year[count],  month[count],  day[count],\
-                                   hour[count],  minute[count])
-            if (cfp[count] > 0):
-                dcfp = float(cfp0-cfp[count-1])/float(cfp[count])
-            else:
-                dcfp = 0
-            if (dcfp > changeInPixCount):
-                print("   Possible hot-tower identified @ ",t2 )
-                print("      Pixels exceeding threshold increased by "+str(dcfp*100.)+\
-                      "% , from "+str(cfp[count-1])+" to "+str(cfp[count])+" pixels")
-                if (mkplot):
-                    levels = np.arange(0, 15000, 1000)
-                    fig = plt.figure(figsize=(10, 8))                
-                    ax1 = fig.add_subplot(2,1,1)
-                    s1 = ax1.contourf(lon, lat, cld_hgt2d[:,:,0], levels, cmap='YlGnBu')
-                    ax1.set(title=t1, ylabel='latitude')
-                    clb1 = fig.colorbar(s1, ax=ax1, shrink=0.9)
-                    clb1.set_label('cloud-top height (m)') 
-                    ax2 = fig.add_subplot(2,1,2)        
-                    s2 = ax2.contourf(lon, lat, cld_hgt2d[:,:,1], levels, cmap='YlGnBu')
-                    ax2.set(title=t2, ylabel='latitude',xlabel='longitude')
-                    clb2 = fig.colorbar(s2, ax=ax2, shrink=0.9)
-                    clb2.set_label('cloud-top height (m)') 
-                    plt.show()
-                    
+            if (count == 1): cld_hgt2d[:,:,1] = cld_hgt
+            if (count > 1):  cld_hgt2d = np.append(cld_hgt2d,  np.atleast_3d(cld_hgt), axis=2)
 
         # Increment counter
         count = count + 1
     else:
         # If not, squack
-        if (debug): print('      Missing day: '+fileList[iTime]+' does not exist')
+        print('      Missing day: '+fileList[iTime]+' does not exist')
 
+# Make plot
+fig = plt.figure()
+ax  = plt.axes()
+anim = animation.FuncAnimation(fig, animate, frames=count-1)
+writer = animation.writers['ffmpeg'](fps=1)
+anim.save(fileOUT+'.mp4',writer=writer,dpi=100)
